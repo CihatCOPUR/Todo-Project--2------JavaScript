e1bfeb9b72877f02b16a3bcf46c980d84bee8f59
Cihat <109276013+CihatCOPUR@users.noreply.github.com>
2023-03-31 23:33:20 +0300

    2

--- array.js
+++ array.js
@@ -1,20 +1,9 @@
-const h1 = document.getElementById("h1");
-const button = document.querySelector("button");
-const remove = document.querySelector("#removebtn");
+const button = document.querySelector("#addBtn");
 const input = document.querySelector("#input");
-const ul = document.querySelector(".ul");
-const newValue = document.createElement("li");
+const ul = document.querySelector("#ul");
 
-h1.onmouseover = () => {
-  h1.style.color = "gold";
-  h1.style.fontSize = "2rem";
-};
-h1.onmouseout = () => {
-  h1.style.color = "black";
-  h1.style.fontSize = "1.9rem";
-};
 button.onclick = () => {
-  if (input.value !== "") {
+  if (input.value.trim() !== "") {
     const li = document.createElement("li");
     li.innerText = input.value;
     ul.appendChild(li);
@@ -22,15 +11,4 @@ button.onclick = () => {
     alert("Enter a value");
   }
   input.value = "";
-};
-
-ul.onmouseover = () => {
-  ul.style.border = "3px solid red";
-};
-ul.onmouseout = () => {
-  ul.style.border = "none";
-};
-
-remove.onclick = () => {
-  ul.lastElementChild.remove();
 };
