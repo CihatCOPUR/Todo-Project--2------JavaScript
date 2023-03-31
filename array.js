2402af5191110b6cb834127dee1c16621567834f
Cihat <109276013+CihatCOPUR@users.noreply.github.com>
2023-04-01 00:13:04 +0300

    5

--- array.js
+++ array.js
@@ -4,11 +4,33 @@ const ul = document.querySelector("#ul");
 
 button.onclick = () => {
   if (input.value.trim() !== "") {
-    const li = document.createElement("li");
-    li.innerText = input.value;
-    ul.appendChild(li);
+    const newTodo = {
+      id: new Date().getTime(),
+      completed: false,
+      text: input.value,
+    };
+    createListElement(newTodo);
   } else {
     alert("Enter a value");
   }
   input.value = "";
+};
+
+const createListElement = (newTodo) => {
+  const li = document.createElement("li");
+  li.id = newTodo.id;
+  const completeIcon = document.createElement("i");
+  completeIcon.className = "fas fa-check";
+  li.appendChild(completeIcon);
+  ul.appendChild(li);
+  console.log(li);
+
+  const p = document.createElement("p");
+  const todoText = document.createTextNode(newTodo.text);
+  p.appendChild(todoText);
+  li.appendChild(p);
+
+  const deleteIcon = document.createElement("i");
+  deleteIcon.className = "fas fa-trash";
+  li.appendChild(deleteIcon);
 };
